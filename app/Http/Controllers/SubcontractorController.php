f9e805d1084f6483c9d83476f16fc6e9ae666bb2
László Molnár <garrotter@gmail.com>
2019-02-16 17:33:38 +0100

    Add Models, Controllers, Migrations

---
+++ app/Http/Controllers/SubcontractorController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Subcontractor;
+use Illuminate\Http\Request;
+
+class SubcontractorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Subcontractor  $subcontractor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Subcontractor $subcontractor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Subcontractor  $subcontractor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Subcontractor $subcontractor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Subcontractor  $subcontractor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Subcontractor $subcontractor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Subcontractor  $subcontractor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Subcontractor $subcontractor)
+    {
+        //
+    }
+}
