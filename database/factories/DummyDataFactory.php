7b0a9c3e824413fe63153868c324dc5b078854dc
László Molnár <garrotter@gmail.com>
2019-02-17 20:27:25 +0100

    Add dummy data

--- database/factories/DummyDataFactory.php
+++ database/factories/DummyDataFactory.php
@@ -2,8 +2,56 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Worker::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->name,
+        'phone' => $faker->unique()->phoneNumber,
+        'email' => $faker->unique()->safeEmail
+    ];
+});
+
+$factory->define(App\Truck::class, function (Faker $faker) {
+    return [
+        'plate' => $faker->unique()->bothify('???-###')
+    ];
+});
+
+$factory->define(App\Subcontractor::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'phone' => $faker->unique()->phoneNumber
+    ];
+});
+
+$factory->define(App\Company::class, function (Faker $faker) {
+    return [
+        'name' => $faker->company,
+        'tax' => $faker->numerify('########-#-##'),
+        'bill_zip' => $faker->numerify('####'),
+        'bill_country' => 'Magyarország',
+        'bill_city' => $faker->city(),
+        'bill_address' => $faker->streetAddress(),
+        'post_zip' => $faker->numerify('####'),
+        'post_country' => 'Magyarország',
+        'post_address' => $faker->streetAddress()
+    ];
+});
+
+$factory->define(App\Employee::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'company_id' => $faker->numberBetween($min = 1, $max = 15),
+        'email' => $faker->unique()->safeEmail,
+        'phone' => $faker->unique()->phoneNumber
+    ];
+});
+
+$factory->define(App\Work::class, function (Faker $faker) {
+    return [
+        'customer_id' => $faker->numberBetween($min = 1, $max = 15),
+        'date' => $faker->dateTimeThisMonth($max = 'now + 7 days'),
+        'description' => $faker->sentence($nbWords = 6, $variableNbWords = true),
+        'lead' => $faker->text($maxNbChars = 50)
     ];
 });
--- database/seeds/DummyDataSeeder.php
+++ database/seeds/DummyDataSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class DummyDataSeeder extends Seeder
 {
@@ -11,6 +12,17 @@ class DummyDataSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $user = new User();
+        $user->name = 'Laci';
+        $user->email = 'laci@laci.hu';
+        $user->password = bcrypt('secret');
+        $user->save();
+
+        $worker = factory(App\Worker::class, 5)->create();
+        $truck = factory(App\Truck::class, 5)->create();
+        $subcontractor = factory(App\Subcontractor::class, 5)->create();
+        $company = factory(App\Company::class, 15)->create();
+        $employee = factory(App\Employee::class, 50)->create();
+        $work = factory(App\Work::class, 50)->create();
     }
 }
