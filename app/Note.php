b8a930d20ca958bc43edb77eb65ea2fa06b50b6b
László Molnár <garrotter@gmail.com>
2019-02-18 17:12:41 +0100

    Edit notes model

--- app/Note.php
+++ app/Note.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Note extends Model
 {
-    //
+    protected $fillable = [
+        'id', 'date', 'note'
+    ];
+
+    protected $hidden = [];
+
+    public $table = "notes";
+    public $timestamps = null;
+
 }
