c8d0c08d730a13ec9af2ea4fb3fdab306fb15fdb
László Molnár <garrotter@gmail.com>
2019-03-27 12:39:57 +0100

    Redirect from /home to /works

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,11 @@ Route::get('/', function () {
     return redirect('works');
 });
 
-Auth::routes();
+Route::get('/home', function () {
+    return redirect('works');
+});
 
-Route::get('/home', 'HomeController@index')->name('home');
+Auth::routes();
 
 Route::get('/works', 'WorkController@index');
 Route::get('/work/new', 'WorkController@create');
