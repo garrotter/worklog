48cf89a3b1a6dcbcc7cc161e5234c5ee8ed6e36b
László Molnár <garrotter@gmail.com>
2019-04-27 16:10:22 +0200

    Encrypted names, phones and e-mails

--- app/Worker.php
+++ app/Worker.php
@@ -3,11 +3,42 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Crypt;
 
 class Worker extends Model
 {
+    protected $fillable = [
+        'name'
+    ];
+
+    protected $hidden = [];
+
     public $table = "workers";
     public $timestamps = null;
+
+    public function getNameAttribute($value) {
+        return Crypt::decrypt($value);
+    }
+
+    public function setNameAttribute($value) {
+        $this->attributes['name'] = Crypt::encrypt($value);
+    }
+
+    public function getPhoneAttribute($value) {
+        return Crypt::decrypt($value);
+    }
+
+    public function setPhoneAttribute($value) {
+        $this->attributes['phone'] = Crypt::encrypt($value);
+    }
+
+    public function getEmailAttribute($value) {
+        return Crypt::decrypt($value);
+    }
+
+    public function setEmailAttribute($value) {
+        $this->attributes['email'] = Crypt::encrypt($value);
+    }
 
     public function work()
     {
